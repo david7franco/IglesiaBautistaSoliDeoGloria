d47122f56bdc87703cf903b323554ec46b01d1a0
David Franco <david7franco@Outlook.com>
2023-10-03 10:16:39 -0400

    added main_page.html and added some django functiionality

---
+++ djangoChurch/churchApp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views 
+from django.views.generic.base import RedirectView
+
+
+urlpatterns = [
+    path('', views.main_page, name='main_page'),
+
+    
+]
